382f8f9c820f30d5cfcb183fab418216e08e9f77
dewaldsteg <58206733+dewaldsteg@users.noreply.github.com>
2020-03-13 22:00:03 +0200

    Change Loading Page

---
+++ Entity-Framework-Core/Migrations/20200313195852_testing migratin after comit.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entity_Framework_Core.Migrations
+{
+    public partial class testingmigratinaftercomit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
